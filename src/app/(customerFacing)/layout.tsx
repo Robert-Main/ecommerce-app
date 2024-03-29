3c2449b28dfcc9eee8af64913c2aad23879447a0
robert-devs <mainrobert04@gmail.com>
2024-03-29 23:46:24 +0300

    adding stripe

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -10,9 +10,16 @@ export default function Layout({
     return (
         <>
             <Nav>
-                <NavLink href="/">Home</NavLink>
-                <NavLink href="/products">Products</NavLink>
-                <NavLink href="/orders">My Orders</NavLink>
+                <div className="flex justify-between my-8">
+                    <div className="items-start">
+                        <h1>logo</h1>
+                    </div>
+                    <div>
+                        <NavLink href="/">Home</NavLink>
+                        <NavLink href="/products">Products</NavLink>
+                        <NavLink href="/orders">My Orders</NavLink>
+                    </div>
+                </div>
             </Nav>
             <div className="container my-6">{children}</div>
         </>
