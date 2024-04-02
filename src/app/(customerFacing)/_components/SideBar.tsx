031816b712da069703706ffcbf8f0f4316ad22b4
robert-devs <mainrobert04@gmail.com>
2024-04-02 17:50:10 +0300

    updates

--- src/app/(customerFacing)/_components/SideBar.tsx
+++ src/app/(customerFacing)/_components/SideBar.tsx
@@ -1,22 +1,22 @@
-import { Nav, NavLink } from "@/components/Nav";
+import { NavLink } from "@/components/Nav";
 
 export const dynamic = "force-dynamic";
 
-export default function Layout({
+export default function SideBar({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <>
-            <Nav>
+        <div className="flex items-center justify-between gap-4 flex-row mx-auto w-full">
+            <div className="flex flex-col justify-center items-center gap-2 bg-red-100 h-full w-[200px] mx-6 fixed left-0">
                 <NavLink href="/">Home</NavLink>
                 <NavLink href="/products">Products</NavLink>
                 <NavLink href="/orders">My Orders</NavLink>
-            </Nav>
+            </div>
             <div className="container bg-gray-600 flex-1 rounded-md py-6 my-6">
                 {children}
             </div>
-        </>
+        </div>
     );
 }
